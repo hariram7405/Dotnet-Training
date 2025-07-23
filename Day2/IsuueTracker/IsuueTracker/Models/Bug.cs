1256e554387a7004084618b27a99195468edf9f2
Hari Ram L <Hariram.L@epssw.com>
2025-07-23 12:04:19 +0530

    Add Day2 files, ignoring .vs folder

---
+++ Day2/IsuueTracker/IsuueTracker/Models/Bug.cs
@@ -0,0 +1,23 @@
+﻿namespace IsuueTracker.Models
+{
+    public class Bug : Issue, IReportable
+    {
+        public string Severity { get; set; }
+
+        public Bug(int id, string title, string assignedto, string severity)
+            : base(id, title, assignedto)
+        {
+            Severity = severity;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"[BUG] #{Id}: {Title} | Severity: {Severity} | Assigned to: {Assignedto}");
+        }
+
+        public void ReportStatus()
+        {
+            Console.WriteLine($"Bug #{Id} is under investigation. It is still in progress.");
+        }
+    }
+}
---
+++ Day2/IsuueTracker/IsuueTracker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using IsuueTracker.Models;
+
+List<Issue> issues = new List<Issue>();
+
+Bug bug1 = new Bug(1, "App crashes on login", "Ravi", "Critical");
+WorkTask task1 = new WorkTask(2, "Page not loading", "Anita", 6);
+
+
+issues.Add(bug1);
+issues.Add(task1);
+
+Console.WriteLine("All Issues:\n");
+
+foreach (Issue issue in issues)
+{
+    issue.Display();
+
+    if (issue is IReportable reportable)
+    {
+        reportable.ReportStatus();
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
