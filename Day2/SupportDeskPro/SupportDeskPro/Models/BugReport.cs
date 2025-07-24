7343e7c28ad0267b2f0f3586b11d34cf0352eae1
Hari Ram L <Hariram.L@epssw.com>
2025-07-24 07:51:16 +0530

    Add SupportDeskPro project to Day2

---
+++ Day2/SupportDeskPro/SupportDeskPro/Models/BugReport.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportDeskPro.Models
+{    public class BugReport :SupportTicket,IReportable
+    {
+        string severity;
+        public BugReport(int id, string title, string description, string createdBy,string severity)
+        : base(id,title, description, createdBy)
+        {
+            this.severity=severity;
+        }
+        public override void DisplayDetails()
+        {
+
+            Console.WriteLine($"Id :{id}| Title: {title}| Description: {description}");
+            Console.WriteLine($"Created By:{createdBy}| status: {status}");
+            Console.WriteLine();
+            
+
+        }
+        public void ReportStatus()
+        {
+            Console.WriteLine("========BUG REPORT==========");
+            Console.WriteLine("---------------------------------------------------------------");
+
+            Console.WriteLine($"Id: {id}");
+            Console.WriteLine($"Title: {title} ");
+            Console.WriteLine($"Description: {description}");
+            Console.WriteLine($"Created By: {createdBy}");
+            Console.WriteLine($"Severity:{severity}");
+            Console.WriteLine(); Console.WriteLine();
+        }
+    }
+}
---
+++ Day2/SupportDeskPro/SupportDeskPro/Models/SupportTicket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportDeskPro.Models
+{
+    public class SupportTicket
+    {
+
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string createdBy { get; set; }
+        public string status { get; set; }
+        public SupportTicket(int id, string title, string description, string createdBy) 
+        {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+            this.createdBy = createdBy;
+            status = "open";
+        }
+
+        public virtual void DisplayDetails()
+        {
+            
+            Console.WriteLine($"Id :{id}| Title: {title}| Description: {description}");
+            Console.WriteLine($"Created By:{createdBy}| status: {status}");
+            Console.WriteLine();
+
+        }
+
+        public void closeTicket(int tid)
+        {
+            status = "closed";
+            Console.WriteLine($"Ticket id {tid} has been closed");
+            Console.WriteLine();
+
+        }
+    }
+}
