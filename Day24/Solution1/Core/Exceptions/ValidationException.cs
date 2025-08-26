0d6a341bcb9ff34d1c7827fb49ceaf21edcc2698
Hari Ram L <Hariram.L@epssw.com>
2025-08-26 14:38:53 +0530

    feat:Added Policy ,enhanced Swagger UI

---
+++ Day24/Solution1/Core/Exceptions/ValidationException.cs
@@ -0,0 +1,17 @@
+﻿namespace BugTracker.Core.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException() : base("Validation failed.")
+        {
+        }
+
+        public ValidationException(string message) : base(message)
+        {
+        }
+
+        public ValidationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
